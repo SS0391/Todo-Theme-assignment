395238f2b0ce7aa0e3b20d58a2292ee159e9f896
SS0391 <ss0391web@gmail.com>
2026-09-17 14:25:09 +0200

    Theme is up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 
 function App() {
-  return;
+  const { theme } = useTheme();
+  return (
+    <div className={`app-container ${theme === "light" ? "app-light" : ""}`}>
+      <h1>useEffect Assignment Theme and Todo List</h1>
+      <ThemeToggle />
+    </div>
+  );
 }
 
 export default App;
