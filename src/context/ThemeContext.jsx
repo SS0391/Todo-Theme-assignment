30bc5f5c86d03cea8663b26575c2ee670e8d1663
SS0391 <ss0391web@gmail.com>
2026-09-17 13:53:46 +0200

    ThemeProvider created

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+// a new context object to make the Theme
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  // Default theme is "Dark"
+  const [theme, setTheme] = useState("dark");
+
+  // Function that lets user switch between dark and light mode
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+}
