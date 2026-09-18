08742557dc842c1fb934105e2806dc9ffc226ae5
SS0391 <ss0391web@gmail.com>
2026-09-18 14:20:35 +0200

    added Done btn X for todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -7,7 +7,11 @@ export function TodoList() {
     <ul className={styles.todoList}>
       {todos.map((todo) => (
         <li key={todo.id} className={`${styles.todoItem} ${todo.done ? styles.completed : ""}`}>
-          <input type="checkbox" checked={todo.done} />
+          <input type="checkbox" checked={todo.done} onChange={() => toggleTodo(todo.id)} className={styles.checkbox} />
+          <span className={styles.checkbox}>{todo.txt}</span>
+          <button onClick={() => removeTodo(todo.id)} className={styles.deleteBtn}>
+            X
+          </button>
         </li>
       ))}
     </ul>
