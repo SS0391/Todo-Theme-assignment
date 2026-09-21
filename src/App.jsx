a4c23ccb545d2f033dc7ec3411002c1ebf6cb6f7
SS0391 <ss0391web@gmail.com>
2026-09-21 08:41:18 +0200

    added Todolist to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import ThemeToggle from "./components/ThemeToggle/ThemeToggle.jsx";
 import { TodoForm } from "./components/TodoForm/TodoForm.jsx";
+import { TodoList } from "./components/TodoList/TodoList";
 import { useTheme } from "./context/ThemeContext.jsx";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1>useEffect Assignment Theme and Todo List</h1>
       <ThemeToggle />
       <TodoForm />
+      <TodoList />
     </div>
   );
 }
